cf25caf34a0daadb737c1c45f003b68ff3cafdf7
Farida Yasser <farida.yasser510@gmail.com>
2023-08-02 02:08:02 +0300

    step-23

---
+++ lib/services/auth/bloc/auth_bloc.dart
@@ -0,0 +1,49 @@
+import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mynotes/services/auth/auth_provider.dart';
+import 'package:mynotes/services/auth/bloc/auth_state.dart';
+
+import 'auth_event.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc(AuthProvider provider) : super(const AuthStateLoading()) {
+    on<AuthEventInitialize>(
+      (event, emit) async {
+        await provider.initialize();
+        final user = provider.currentUser;
+        if (user == null) {
+          emit(const AuthStateLoggedOut());
+        } else if (!user.isEmailVerified) {
+          emit(const AuthStateNeedsVerification());
+        } else {
+          emit(AuthStateLoggedIn(user));
+        }
+      },
+    );
+
+    on<AuthEventLogIn>(
+      (event, emit) async {
+        try {
+          emit(const AuthStateLoading());
+          final user = await provider.logIn(
+              email: event.email, password: event.password);
+          emit(AuthStateLoggedIn(user));
+        } on Exception catch (e) {
+          emit(AuthStateLoginFailure(e));
+        }
+      },
+    );
+
+    on<AuthEventLogOut>(
+      (event, emit) async {
+        try {
+          emit(const AuthStateLoading());
+          await provider.logOut();
+          emit(AuthStateLoggedOut());
+        } on Exception catch (e) {
+          emit(AuthStateLogoutFailure(e));
+        }
+      },
+    );
+  }
+}
--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:mynotes/services/auth/bloc/auth_event.dart';
 import 'package:mynotes/services/cloud/cloud_note.dart';
 import 'package:mynotes/services/cloud/firebase_cloud_storage.dart';
 import 'package:mynotes/views/notes/notes_list_view.dart';
 import '../../constants/routes.dart';
 import '../../enums/menu_action.dart';
+import '../../services/auth/bloc/auth_bloc.dart';
 import '../../utilities/dialogs/logout_dialog.dart';
 
 class NotesView extends StatefulWidget {
@@ -49,9 +52,7 @@ class _NotesViewState extends State<NotesView> {
                   case MenuAction.logout:
                     final shouldLogout = await showLogoutDialog(context);
                     if (shouldLogout) {
-                      await AuthService.firebase().logOut();
-                      Navigator.of(context)
-                          .pushNamedAndRemoveUntil(loginRoute, (_) => false);
+                      context.read<AuthBloc>().add(const AuthEventLogOut());
                     }
                 }
               },
